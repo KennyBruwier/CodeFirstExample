bfa49248b47ca110f9303989e2933f0f260de743
Kenny Bruwier <kenny.bruwier@gmail.com>
2021-04-30 09:19:47 +0100

    Add project files.

---
+++ CodeFirstExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("starting...");
+            using(var ctx = new NotitieboekjeContext())
+            {
+                ctx.Notitieboekjes.Add(new Notitieboekje() { Naam = "Dagboek" }) ;
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("end");
+            Console.ReadLine();
+        }
+    }
+    public class Notitieboekje
+    {
+        public int NotitieboekjeId { get; set; }
+        public string Naam { get; set; }
+    }
+    public class Pagina
+    {
+        public int PaginaId { get; set; }
+        public string Tekst { get; set; }
+        public Notitieboekje Notitieboekje { get; set; }
+    }
+
+    public class NotitieboekjeContext : DbContext
+    {
+        public NotitieboekjeContext() : base("name=NotitieboekjeDBConnectionString")
+        {
+
+        }
+
+        public DbSet<Notitieboekje> Notitieboekjes { get; set; }
+        public DbSet<Pagina> Paginas { get; set; }
+    }
+}
